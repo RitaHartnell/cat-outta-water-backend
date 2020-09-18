fe18270dd3d95a2903725114e09087420fac8b5a
RitaHartnell <mel.soriano6123@gmail.com>
2020-09-18 14:35:34 -0400

    jwt auth backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :users, only: [:create]
+        post '/login', to: 'auth#create'
+        get '/profile', to: 'users#profile'
+      end
+    end
+  end
 end
