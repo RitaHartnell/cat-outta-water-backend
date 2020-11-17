1a528101bbc27894246f45a3c9172561f796846e
Melody Soriano <mel.soriano6123@gmail.com>
2020-11-16 19:22:48 -0500

    oops

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -41,6 +41,6 @@ class Api::V1::UsersController < ApplicationController
     
     private
         def user_params
-            params.require(:user).permit(:username, :password, :bio, :avatar)
+            params.require(:user).permit(:username, :password, :bio, :avatar, :save_state)
         end
 end
